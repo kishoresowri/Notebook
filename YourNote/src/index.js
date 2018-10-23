97454febdaebe65e51445256db8449ef8cf45f0f
kishore <sowri.kishore@gmail.com>
2018-10-23 22:18:46 +0530

    updated

--- YourNote/src/index.js
+++ YourNote/src/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $("#add-note").on("click", function() {
-		var txt = "<input >";
-		$("li").append(txt);
+		var txt = document.getElementById("note-list");
+		$("li").append(txt).css({ width: "150px", height: "100px", border: "2px solid grey"});
   });
 });
 
